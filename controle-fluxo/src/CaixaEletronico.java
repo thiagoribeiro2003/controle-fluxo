291ea87b2edc14d0a77d92f7923b3d45e4a7e787
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2023-09-25 01:18:50 -0300

    Exemplos de condição composta

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -2,11 +2,17 @@ public class CaixaEletronico {
     public static void main(String[] args) {
         // CaixaEletronico.java
        double saldo = 25.0;
-       double valorSolicitado = 26.0;
+       double valorSolicitado = 16.0;
 
-       if(valorSolicitado < saldo)
-        saldo = saldo - valorSolicitado;
+       if(valorSolicitado < saldo) {
+            saldo = saldo - valorSolicitado;
+            System.out.println("Novo saldo: " + saldo);
+        }
+        else {
+            System.out.println("Saldo insuficiente");
+        }
+        
 
-        System.out.println(saldo);
+       
     }
 }
