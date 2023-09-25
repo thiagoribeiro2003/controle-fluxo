6f3b7eb16e8bdab8bfa63c5aa0afc4f25ba0003a
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2023-09-25 01:27:03 -0300

    Exemplo de condição encadeada

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -2,13 +2,15 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
 
-       int nota = 6 ;
+       int nota = 10;
        
        if(nota >= 7) {
-        System.out.println("Aprovado");
+            System.out.println("Aprovado");
+       } else if(nota >=5 && nota<7){ // true or false
+            System.out.println("Recuperação");
        } else {
-        System.out.println("Reprovado");
+            System.out.println("Reprovado");
        }
-       
+
     }
 }
