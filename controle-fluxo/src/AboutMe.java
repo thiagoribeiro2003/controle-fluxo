2b98b887763ff6ca7a177c297cdac05f12a5ebc0
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2023-09-26 15:41:29 -0300

    Liganco com exceções usando o try/catch

---
+++ controle-fluxo/src/AboutMe.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+        try {
+            System.out.print("Digite seu nome:");
+            String nome = sc.next();
+
+            System.out.print("Digite seu sobrenome:");
+            String sobrenome = sc.next();
+
+            System.out.print("Digite sua idade:");
+            int idade = sc.nextInt();
+
+            System.out.print("Digte sua altura:");
+            double altura = sc.nextDouble();
+
+            //imprimindo os dados obtidos pelo usuario
+            System.out.println("Olá, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
+            System.out.println("Tenho " + idade + " anos ");
+            System.out.println("Minha altura é " + altura + "cm ");
+            sc.close();
+        } catch(java.util.InputMismatchException e) {
+            System.out.println("Os campos idade e altura devem ser númericos");
+        }
+    }
+}
